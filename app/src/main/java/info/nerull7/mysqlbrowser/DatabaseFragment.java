a1ae369973ec01e9c1e547bb1f8feb5f00637a2c
Przemek Grondek <github@nerull7.info>
2014-07-14 11:09:23 +0200

    Lista db
    Jakieś statyczne dane + Standarodwy Array Adapter

--- app/src/main/java/info/nerull7/mysqlbrowser/DatabaseFragment.java
+++ app/src/main/java/info/nerull7/mysqlbrowser/DatabaseFragment.java
@@ -1,20 +1,36 @@
 package info.nerull7.mysqlbrowser;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import info.nerull7.mysqlbrowser.db.DatabaseConnector;
 
 /**
  * Created by nerull7 on 14.07.14.
  */
 public class DatabaseFragment extends Fragment {
+    ListView databasesListView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_database, container, false);
+        View V = inflater.inflate(R.layout.fragment_database, container, false);
+        databasesListView = (ListView)V.findViewById(R.id.databaseList);
+        setupListViewDatabase();
+        return V;
+    }
+
+    private void setupListViewDatabase(){
+        // TODO not local constuction!!!
+        DatabaseConnector databaseConnector = new DatabaseConnector("FIXME","FIXME","FIXME");
+        ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, databaseConnector.getDatabases());
+        databasesListView.setAdapter(listAdapter);
     }
 }
