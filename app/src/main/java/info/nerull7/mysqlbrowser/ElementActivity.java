f5b99dbc71fa79c64a002750b2c6ce296cba17dc
Przemek Grondek <github@nerull7.info>
2014-08-06 18:27:03 +0200

    Warning message on not saving
     Warning message when you cancel saving new entry into database

--- app/src/main/java/info/nerull7/mysqlbrowser/ElementActivity.java
+++ app/src/main/java/info/nerull7/mysqlbrowser/ElementActivity.java
@@ -1,6 +1,10 @@
 package info.nerull7.mysqlbrowser;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,5 +28,27 @@ public class ElementActivity extends Activity {
                     .add(R.id.container, elementFragment)
                     .commit();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.error_no_save));
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setTitle(R.string.warning);
+        builder.setIcon(R.drawable.ic_action_warning);
+        builder.create();
+        builder.show();
     }
 }
